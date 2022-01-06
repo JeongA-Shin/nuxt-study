3cae826cd2e9f928fd857023c859434c0186f9e7
신정아 <shinjeonga@twentyoz.kr>
2022-01-06 18:21:30 +0900

    multi-word-component-names 에러 해결

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <nuxt /> <!-- vue의 <router-view>역할-->
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+body{
+  display: flex;
+  justify-content: center;
+}
+#app{
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+</style>
