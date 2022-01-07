a09a06f537c08bb8ac68111cd6cd691c37c58b9a
신정아 <shinjeonga@twentyoz.kr>
2022-01-07 16:50:07 +0900

    universal deployment exported

--- services/EventService.js
+++ services/EventService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient=axios.create({
   //.create() 메서드를 사용해 사용자 정의 구성을 사용하는 axios 인스턴스를 생성할 수 있습니다.
-  baseURL:`http://localhost:3000`,
+  baseURL:`https://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
   withCredentials:false,
   headers:{
     Accept:'application/json',
