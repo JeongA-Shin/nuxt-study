a0e45715dca04c5a7a695180ce276287ac52ca96
신정아 <shinjeonga@twentyoz.kr>
2022-01-07 15:05:55 +0900

    API calls with AXIOS and display

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Events</h1>
+
+    <!--events는 26 번 라인 참고-->
+    <!--10번의 :even에서 바인딩되는 "event"는 v-for에서 얻은  각 event들임-->
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
+</template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  },
+  // 컴포넌트가 만들어지기 전 미리 api를 통해 필요한 데이터들을 받아옴
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.'
+        })
+      })
+  },
+  components: {
+    EventCard
+  }
+}
+</script>
