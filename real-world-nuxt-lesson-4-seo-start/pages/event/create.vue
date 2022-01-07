912eafec073bc211bac78fe59bf76f7445848209
신정아 <shinjeonga@twentyoz.kr>
2022-01-07 11:22:01 +0900

    ãSE0 처리

---
+++ real-world-nuxt-lesson-4-seo-start/pages/event/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  // SEO를 위한 것
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        // html에서 메타 태그로 들어가게 됨
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create a new event in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
